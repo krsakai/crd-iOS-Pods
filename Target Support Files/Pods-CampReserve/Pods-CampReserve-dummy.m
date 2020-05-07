85290ad9d635d01ca7f6c04917b2aa0d84a213dc
酒井邦也 <k_sakai@waku-2.com>
2020-05-07 10:57:49 +0900

    initialize

---
+++ Target Support Files/Pods-CampReserve/Pods-CampReserve-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CampReserve : NSObject
+@end
+@implementation PodsDummy_Pods_CampReserve
+@end
---
+++ Target Support Files/Pods-CampReserveTests/Pods-CampReserveTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CampReserveTests : NSObject
+@end
+@implementation PodsDummy_Pods_CampReserveTests
+@end
